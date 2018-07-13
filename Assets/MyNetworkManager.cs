e30dabaee5bc70c8897d8236737f7c1c7fb10ca0
James Tietjen <35675497+MrPhuzzles@users.noreply.github.com>
2018-07-13 17:27:25 -0400

    Overriding Keyword
    Altered MyHUD to MyNetworkManager and cleaned up the code inside.

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class MyNetworkManager : NetworkManager {
+
+	
+
+    public void MyStartHost() {
+        Debug.Log("Starting Host at " + Time.timeSinceLevelLoad);
+        StartHost();
+    }
+
+    public override void OnStartHost() {
+        Debug.Log("Host started at " + Time.timeSinceLevelLoad);
+    }
+}
